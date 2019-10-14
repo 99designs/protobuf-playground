1cdfe2fc82dea9c1ee4798998408de70f50effb1
Mathew Byrne <mathew.byrne@99designs.com>
2019-10-14 17:16:07 +1100

    Reworked types to use protobufjs lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,76 @@
 import React from 'react';
 import './App.css';
-import { namespaces } from './model';
+
+import data from './proto.json';
+import protobuf from 'protobufjs';
+
+const root = protobuf.Root.fromJSON(data);
+const sdk = root.lookup('ninety_nine.sdk') as protobuf.Namespace;
+console.log(sdk.lookup('EmptyBrief'));
+
+const byName = (a: { name: string }, b: { name: string }) =>
+  a.name.localeCompare(b.name);
+
+const services = (namespace: protobuf.Namespace): protobuf.Service[] => {
+  const services = namespace.nestedArray.filter(
+    (o): o is protobuf.Service => o instanceof protobuf.Service
+  );
+  services.sort(byName);
+  return services;
+};
+
+const messages = (namespace: protobuf.Namespace): protobuf.Type[] => {
+  const messages = namespace.nestedArray.filter(
+    (o): o is protobuf.Type => o instanceof protobuf.Type
+  );
+  messages.sort(byName);
+  return messages;
+};
+
+const enums = (namespace: protobuf.Namespace): protobuf.Enum[] => {
+  const enums = namespace.nestedArray.filter(
+    (o): o is protobuf.Enum => o instanceof protobuf.Enum
+  );
+  enums.sort(byName);
+  return enums;
+};
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      {namespaces.map(namespace => (
-        <div>
-          <h1>{namespace.name}</h1>
-          <h2>Services</h2>
-          {namespace.services.map(({ name, methods }) => (
-            <>
-              <div>{name}</div>
-              <ol>
-                {methods.map(({ name }) => (
-                  <div>{name}</div>
-                ))}
-              </ol>
-            </>
-          ))}
-          <h2>Messages</h2>
-          {namespace.messages.map(({ name, ...value }) => (
-            <div>{name}</div>
-          ))}
-          <h2>Enums</h2>
-          {namespace.enums.map(({ name, ...value }) => (
-            <div>{name}</div>
-          ))}
-        </div>
-      ))}
+      {sdk &&
+        sdk.nestedArray.map(namespace => (
+          <div key={`ninety_nine.sdk.${namespace.name}`}>
+            <h1>{namespace.name}</h1>
+            <h2>Services</h2>
+            {services(namespace as protobuf.Namespace).map(
+              ({ name, methods }) => (
+                <div>
+                  <div>
+                    {name} {}
+                  </div>
+                  <ol>
+                    {Object.keys(methods).map(name => (
+                      <div>{name}</div>
+                    ))}
+                  </ol>
+                </div>
+              )
+            )}
+            <h2>Messages</h2>
+            {messages(namespace as protobuf.Namespace).map(
+              ({ name, ...value }) => (
+                <div>{name}</div>
+              )
+            )}
+            <h2>Enums</h2>
+            {enums(namespace as protobuf.Namespace).map(
+              ({ name, ...value }) => (
+                <div>{name}</div>
+              )
+            )}
+          </div>
+        ))}
     </div>
   );
 };
