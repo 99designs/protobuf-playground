110b6a0b7b0fe17087cf4cb3d4ef3879e5de73ff
Mathew Byrne <mathew.byrne@99designs.com>
2019-10-14 16:09:56 +1100

    Exploring using custom types on frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import './App.css';
+import { namespaces } from './model';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {namespaces.map(namespace => (
+        <div>
+          <h1>{namespace.name}</h1>
+          <h2>Services</h2>
+          {namespace.services.map(({ name, methods }) => (
+            <>
+              <div>{name}</div>
+              <ol>
+                {methods.map(({ name }) => (
+                  <div>{name}</div>
+                ))}
+              </ol>
+            </>
+          ))}
+          <h2>Messages</h2>
+          {namespace.messages.map(({ name, ...value }) => (
+            <div>{name}</div>
+          ))}
+          <h2>Enums</h2>
+          {namespace.enums.map(({ name, ...value }) => (
+            <div>{name}</div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
