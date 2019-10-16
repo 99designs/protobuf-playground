e6e3ddc9be5a3cb1a234d92192cfdd8de3f829e1
Mathew Byrne <mathew.byrne@99designs.com>
2019-10-16 22:08:45 +1100

    Update generate to import external proto
    Just supporting some static Google imports for now. Can be expanded in
    the future if we need a more generic import solution. It looks like
    protobuf.js does have some sort of dynamic importing ability?

--- generate.js
+++ generate.js
@@ -1,5 +1,6 @@
 const protobufjs = require('protobufjs');
 const fs = require('fs');
+const request = require('request-promise');
 
 const walker = predicate => {
   const walk = (dir, files = []) => {
@@ -19,16 +20,45 @@ const walker = predicate => {
 const protoWalk = walker(file => file.endsWith('.proto'));
 const sources = protoWalk('../proto/');
 
+const parseOptions = {
+  keepCase: true,
+  alternateCommentMode: true, // Important so that it parses trailing comments and non-doc-blocks
+};
+
 const root = new protobufjs.Root();
+
 sources.forEach(path => {
   source = fs.readFileSync(path);
-  protobufjs.parse(source, root, {
-    keepCase: true,
-    alternateCommentMode: true, // Important so that it parses trailing comments and non-doc-blocks
+  protobufjs.parse(source, root, parseOptions);
+  console.log('adding:', path);
+});
+
+// TODO extract these external dependencies from source files somehow
+const extern = [
+  'google/protobuf/field_mask.proto',
+  'google/protobuf/struct.proto',
+  'google/protobuf/timestamp.proto',
+  'google/protobuf/wrappers.proto',
+];
+
+const requests = extern.map(path => {
+  const url =
+    'https://raw.githubusercontent.com/protocolbuffers/protobuf/master/src/' +
+    path;
+  return request(url, (error, _, source) => {
+    if (error) {
+      console.error(error);
+      process.exit(1);
+    }
+    protobufjs.parse(source, root, parseOptions);
+    console.log('external:', path);
   });
 });
 
-fs.writeFileSync(
-  'src/proto.json',
-  JSON.stringify(root.toJSON({ keepComments: true }))
-);
+Promise.all(requests).then(() => {
+  fs.writeFileSync(
+    'src/proto.json',
+    JSON.stringify(root.toJSON({ keepComments: true }))
+  );
+  console.log('wrote: src/proto.json');
+});
