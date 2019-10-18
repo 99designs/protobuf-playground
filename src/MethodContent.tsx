ea54fd30706580cab04406ec8f6276c1db5ebdc3
Mathew Byrne <mathew.byrne@99designs.com>
2019-10-18 16:06:18 +1100

    Add tab selection for playground

--- src/MethodContent.tsx
+++ src/MethodContent.tsx
@@ -1,42 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import protobuf from 'protobufjs';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
 import Box from '@material-ui/core/Box';
+import Divider from '@material-ui/core/Divider';
 import Forward from '@material-ui/icons/Forward';
 import Reply from '@material-ui/icons/Reply';
 import MessageTable from './MessageTable';
 import Breadcrumbs from './Breadcrumbs';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 const MethodContent: React.FC<{ method: protobuf.Method }> = ({ method }) => {
+  const [tab, setTab] = useState(0);
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setTab(newValue);
+  };
   return (
     <div>
       <Breadcrumbs object={method} />
 
       <Box m={4} />
 
-      <Typography variant="h3" gutterBottom>
+      <Typography variant="h4" gutterBottom>
         {method.name}
       </Typography>
 
-      <Divider />
-      <Box m={2} />
-
       {method.comment && <Typography paragraph>{method.comment}</Typography>}
 
-      <Typography variant="h5" gutterBottom>
-        <Forward /> {method.requestType}
-      </Typography>
-      {method.resolvedRequestType && (
-        <MessageTable message={method.resolvedRequestType} />
-      )}
+      <Tabs value={tab} onChange={handleChange}>
+        <Tab label="Description" />
+        <Tab label="Playground" />
+      </Tabs>
+      <Divider />
+      <Box m={4} />
 
-      <Typography variant="h5" gutterBottom>
-        <Reply /> {method.responseType}
-      </Typography>
-      {method.resolvedResponseType && (
-        <MessageTable message={method.resolvedResponseType} />
+      {tab === 0 && (
+        <>
+          <Typography variant="h6" gutterBottom>
+            <Forward fontSize="inherit" /> {method.requestType}
+          </Typography>
+          {method.resolvedRequestType && (
+            <MessageTable message={method.resolvedRequestType} />
+          )}
+
+          <Typography variant="h6" gutterBottom>
+            <Reply fontSize="inherit" /> {method.responseType}
+          </Typography>
+          {method.resolvedResponseType && (
+            <MessageTable message={method.resolvedResponseType} />
+          )}
+        </>
       )}
+      {tab === 1 && <></>}
     </div>
   );
 };
